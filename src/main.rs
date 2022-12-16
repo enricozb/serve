ce5928fbbd6316773be80f253524add2243ffd29
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-12-16 09:09:52 -1000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use indoc::formatdoc;
+use poem::{
+  error::{InternalServerError, NotFoundError},
+  get, handler,
+  listener::TcpListener,
+  web::{Data, Html, Path, StaticFileRequest},
+  EndpointExt, IntoResponse, Response, Result, Route, Server,
+};
+
+/// Serve static files over HTTP.
+#[derive(Clone, Parser)]
+#[command(author, version, about)]
+struct Args {
+  /// The file or directory to serve.
+  path: PathBuf,
+
+  /// The port to serve on.
+  #[arg(long, default_value_t = 8000)]
+  port: u16,
+
+  /// The host to serve on.
+  #[arg(long, default_value_t = String::from("0.0.0.0"))]
+  host: String,
+}
+
+/// Serves a directory or file.
+#[handler]
+fn serve(Path(path): Path<PathBuf>, Data(dir): Data<&PathBuf>, req: StaticFileRequest) -> Result<Response> {
+  let file = dir.join(path);
+
+  if file.is_file() {
+    Ok(req.create_response(file, true)?.into_response())
+  } else if file.is_dir() {
+    let files: Vec<String> = file
+      .read_dir()
+      .map_err(InternalServerError)?
+      .into_iter()
+      .filter_map(|entry| entry.ok().map(|entry| entry.file_name()))
+      .map(|file| {
+        formatdoc! {"
+          <li>
+            <a href=\"{file}\">{file}</a>
+          </li>
+        ",
+          file = file.to_string_lossy()
+        }
+      })
+      .collect();
+
+    Ok(
+      Html(formatdoc! {"
+          <!DOCTYPE html>
+          <html>
+          <head>
+            <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">
+            <title>Directory {dir}</title>
+          </head>
+          <body>
+            <h1>Directory {dir}</h1>
+            <hr>
+            <ul>
+              {files}
+            </ul>
+            <hr>
+          </body>
+        </html>
+      ",
+        dir = dir.to_string_lossy(),
+        files = files.join("\n"),
+      })
+      .into_response(),
+    )
+  } else {
+    Err(NotFoundError.into())
+  }
+}
+
+/// Serves a single file.
+#[handler]
+fn serve_file(Data(file): Data<&PathBuf>, req: StaticFileRequest) -> Result<impl IntoResponse> {
+  Ok(req.create_response(file, true)?)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+  let args = Args::parse();
+
+  let app = if args.path.is_file() {
+    Route::new().at("/", get(serve_file)).data(args.path)
+  } else {
+    Route::new().at("/*path", get(serve)).data(args.path)
+  };
+
+  Server::new(TcpListener::bind((args.host, args.port))).run(app).await
+}
